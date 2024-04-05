e96b977ef831541cdf330882539526737c96e572
Timo Dehandschutter <timo.dehandschutter@gmail.com>
2024-04-05 17:15:27 +0200

    all tests seem to work

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use piece_table::PieceTable;
+
+mod piece_table;
+
+fn main() -> Result<(), String> {
+    let original = String::from("I am a piece table!");
+
+    let mut piece_table = PieceTable::new(original);
+
+    // insert values at the start, somewhere in the middle and at the end
+    piece_table.insert(0, "start ")?;
+    piece_table.insert(12, " somewhere in the middle")?;
+    piece_table.insert(piece_table.len(), " end")?;
+
+    // delete last char, first char, and a char somewhere in the middle
+    piece_table.delete(piece_table.len() - 1)?;
+    piece_table.delete(30)?;
+    piece_table.delete(0)?;
+
+    // delete a range of values
+    piece_table.delete_range(0, 5)?;
+
+    // generate a string from the piece table
+    println!("{}", piece_table.generate_string());
+
+    // get the length of the piece table
+    println!("{}", piece_table.len());
+
+    Ok(())
+}
